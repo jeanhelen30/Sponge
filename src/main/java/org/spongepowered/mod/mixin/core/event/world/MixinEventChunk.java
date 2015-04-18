6e69eb7d98da00bba8e60ce41a492db002bdd8d1
Minecrell <dev@minecrell.net>
2015-04-18 16:07:54 +0200

    Fix mixin problem

--- src/main/java/org/spongepowered/mod/mixin/core/event/world/MixinEventChunk.java
+++ src/main/java/org/spongepowered/mod/mixin/core/event/world/MixinEventChunk.java
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.mod.mixin.core.event.chunk;
+package org.spongepowered.mod.mixin.core.event.world;
 
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.WorldEvent;
